e7c292247ef8b89cc1af224905f2f823f75047b1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-28 17:04:29 +0300

    store component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Routes from './Routes/Routes';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (localStorage.getItem('jwtToken')) {
+  setAuthToken(localStorage.getItem('jwtToken'));
+  const userDecoded = jwt_decode(localStorage.getItem('jwtToken'));
+  store.dispatch(setCurrentUser(userDecoded));
+  // Check for expired time
+  const currentTime = Date.now() / 1000;
+  if (userDecoded.exp < currentTime) {
+    // Logout
+    store.dispatch(logoutUser());
+    // User
+    store.dispatch(setCurrentUser({}));
+    window.location.href = "/";
+  }
+
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Routes />
+      </div>
+    );
+  }
 }
 
 export default App;
